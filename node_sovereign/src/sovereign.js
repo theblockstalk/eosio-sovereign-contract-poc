c1e723e2e2242373068bd4d047fc3a05e7a8e2b1
Jack Tanner <jack@jackandtheblockstalk>
2019-12-18 21:53:29 +0100

    i64 type not supported

--- node_sovereign/src/sovereign.js
+++ node_sovereign/src/sovereign.js
@@ -1,6 +1,8 @@
 const wasm = require('./eosio_wasm');
 
-const CONTRACT_WASM_PATH = '../build/sovereign.wasm';
+const CONTRACT_WASM_PATH = '../build/sovereign.wasm'; // i64 types are not supported!
+// TypeError: wasm function signature contains illegal type
+
 const CONTRACT_ABI_PATH = '../build/sovereign.abi';
 
 module.exports.hi = async function () {
