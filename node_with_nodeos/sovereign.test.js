a1e11809c3cfa7b958241b0791d9660aa92c2a82
Jack Tanner <jack@jackandtheblockstalk>
2019-12-18 19:05:57 +0100

    nodeos test passing

---
+++ node_with_nodeos/sovereign.test.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const eoslime = require('eoslime').init('local');
+const eos = eoslime.Provider.eos;
+
+const CONTRACT_WASM_PATH = '../build/sovereign.wasm';
+const CONTRACT_ABI_PATH = '../build/sovereign.abi';
+
+describe('sovereign', function () {
+    // Increase mocha(testing framework) time, otherwise tests fails
+    this.timeout(15000);
+
+    let contractAccount;
+    let myAccount;
+    
+    before(async () => {
+        let accounts = await eoslime.Account.createRandoms(1);
+        myAccount = accounts[0];
+    });
+
+    beforeEach(async () => {
+        contractAccount = await eoslime.Contract.deploy(CONTRACT_WASM_PATH, CONTRACT_ABI_PATH);
+    });
+
+    it('Should store the hash', async () => {        
+        const tx = await contractAccount.hi();
+        
+        console.log(tx);
+    });
+});
