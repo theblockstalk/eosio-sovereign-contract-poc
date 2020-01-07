114157e040c7ff2dbf4143ee0cf8d5911a0c9664
Jack Tanner <jack@jackandtheblockstalk.com>
2020-01-07 17:31:42 +0100

    cleaned up

--- sovereign/src/sovereign.js
+++ sovereign/src/sovereign.js
@@ -2,10 +2,7 @@ const fs = require('fs');
 const contract = require('./eosio/contract');
 const name = require('./eosio/name');
 
-// const CONTRACT_WASM_PATH = '../build/sovereign.wasm'; // i64 types are not supported!
-// TypeError: wasm function signature contains illegal type
-
-const CONTRACT_WASM_FILE = fs.readFileSync('../build/sovereign.wasm');; // i64 types converted to i32
+const CONTRACT_WASM_FILE = fs.readFileSync('../build/sovereign.wasm');
 const CONTRACT_ABI_FILE = fs.readFileSync('../build/sovereign.abi');
 
 module.exports.hi = async function () {
@@ -13,6 +10,7 @@ module.exports.hi = async function () {
     const sovereign_contract = await contract.contract(CONTRACT_WASM_FILE, CONTRACT_ABI_FILE);
     
     console.log("calling apply function");
+
     const receiver = name("sovereign");
     const code = name("dablockstalk");
     const action = name("hi")
